a5eca96c8af161325cd65e570a6ba131c89c93dd
Akeem <100803916+Akbram98@users.noreply.github.com>
2023-05-11 18:15:05 -0400

    Working on UI for Patient Information Dashboard - incomplete

---
+++ app/src/main/java/com/example/paramed/ParaPatientDashboard.java
@@ -0,0 +1,14 @@
+package com.example.paramed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ParaPatientDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_para_patient_dashboard);
+    }
+}
---
+++ app/src/main/java/com/example/paramed/PatientInfo.java
@@ -0,0 +1,14 @@
+package com.example.paramed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_info);
+    }
+}
